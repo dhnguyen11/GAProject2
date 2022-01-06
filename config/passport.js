ea2fe24c4232c001476d9c3480955e74db7c5d2f
dhnguyen01 <davidhuongnguyen@gmail.com>
2022-01-06 14:11:16 -0800

    Initial passport setup

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 //Require your User Model here!
+const User = require('../models/user');
 
 // configuring Passport!
 passport.use(new GoogleStrategy({
@@ -11,7 +12,23 @@ passport.use(new GoogleStrategy({
   function(accessToken, refreshToken, profile, cb) {
     // a user has logged in via OAuth!
     // refer to the lesson plan from earlier today in order to set this up
-
+    User.findOne({ 'googleId': profile.id }, function(err, user) {
+      if (err) return cb(err);
+      if (user) {
+        return cb(null, user);
+      }
+      else {
+        var newUser = new User({
+          name: profile.displayName,
+          email: profile.emails[0].value,
+          googleId: profile.id
+        });
+        newUser.save(function(err) {
+          if (err) return cb(err);
+          return cb(null, newUser);
+        })
+      }
+    })
   }
 ));
 
@@ -24,7 +41,9 @@ passport.deserializeUser(function(id, done) {
   // Find your User, using your model, and then call done(err, whateverYourUserIsCalled)
   // When you call this done function passport assigns the user document to req.user, which will 
   // be availible in every Single controller function, so you always know the logged in user
-
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
 });
 
 
--- models/user.js
+++ models/user.js
@@ -8,4 +8,6 @@ const userSchema = new mongoose.Schema({
     googleId: String
 }, {
     timestamps: true
-})
+});
+
+module.exports = mongoose.model('User', userSchema);
