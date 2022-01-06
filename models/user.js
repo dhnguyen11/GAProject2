0b53a38acd1fefda95883dc8ff4f4d70ab0f71bc
dhnguyen01 <davidhuongnguyen@gmail.com>
2022-01-06 13:53:54 -0800

    Beginning User Schema

--- models/user.js
+++ models/user.js
@@ -1,3 +1,11 @@
 const mongoose = require('mongoose');
 
 // Create your User Model
+// User model MUST include the googleId, like in the passport example app
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    googleId: String
+}, {
+    timestamps: true
+})
