b507a0e91c9b1b8bf31506d0572caf39cde2017a
dhnguyen01 <davidhuongnguyen@gmail.com>
2022-01-12 10:42:06 -0800

    Adding route for update function

--- controllers/members.js
+++ controllers/members.js
@@ -6,6 +6,8 @@ const request = require("request");
 module.exports = {
   new: newMember,
   create,
+  edit,
+  update
 };
 
 function newMember(req, res) {
@@ -81,4 +83,60 @@ function create(req, res) {
   } else {
     res.redirect("/");
   }
+}
+
+function edit(req, res) {
+  if (req.user) {
+    Member.findById(req.params.id, function (err, member) {
+      let capName = member.name.charAt(0).toUpperCase() + member.name.substr(1);
+      member.capName = capName;
+      request(
+        `${process.env.BASE_URL}pokemon/${member.name}`,
+        function (err, request, body) {
+          const pkmnData = JSON.parse(body);
+          const moveList = pkmnData.moves;
+          const baseMoves = [];
+          moveList.forEach((m) => {
+            for (let v of m.version_group_details) {
+              if (v.version_group.name === "red-blue") {
+                baseMoves.push({ name: m.move.name });
+                break;
+              }
+            }
+          });
+          baseMoves.forEach((m) => {
+            const words = m.name.split("-");
+            for (let i = 0; i < words.length; i++) {
+              words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+            }
+            moveName = words.join(" ");
+            m.capName = moveName;
+          });
+          res.render("members/edit", {
+            title: `Edit ${member.capName}`,
+            moves: baseMoves,
+            member,
+          });
+        }
+      );
+    });
+  } else {
+    res.redirect("/");
+  }
+}
+
+function update(req, res) {
+  console.log(req.body, "<- req.body");
+  console.log(req.params, "<- req.params");
+  console.log(req.query, "<- req.query");
+  req.body.moves = [];
+  req.body.moves.push(req.body.move1);
+  req.body.moves.push(req.body.move2);
+  req.body.moves.push(req.body.move3);
+  req.body.moves.push(req.body.move4);
+  delete req.body.move1;
+  delete req.body.move2;
+  delete req.body.move3;
+  delete req.body.move4;
+  res.send("hitting route");
 }
--- routes/members.js
+++ routes/members.js
@@ -5,5 +5,7 @@ const membersCtrl = require('../controllers/members');
 // Routes
 router.get('/teams/:teamId/members', membersCtrl.new);
 router.post('/teams/:teamId/members/:name', membersCtrl.create);
+router.get('/teams/members/:id', membersCtrl.edit);
+router.put('/teams/members/:id/:name', membersCtrl.update);
 
 module.exports = router;
